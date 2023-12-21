b2ba89d33760889ddfca7d69f71b7e3ca35b59a6
Anthony Tsang <htsang@student.42heilbronn.de>
2023-12-21 16:51:56 +0100

    fix(mariadb): mariadb also having a wordpress database on initialization

--- srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mariadb <<EOF
+mariadbd --user=mysql <<EOF
 CREATE DATABASE ${WP_DB_NAME};
 CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
