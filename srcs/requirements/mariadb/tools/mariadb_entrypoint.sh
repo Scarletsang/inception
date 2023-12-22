1f2f2cced0492f95557d0ea3b100239de7a2eb27
Anthony Tsang <anthonytsang0226@gmail.com>
2023-12-22 19:03:59 +0100

    fix(mariadb & wordpress): mariadb works now and wordpress can send sql query to mariadb

--- srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-mariadbd --user=mysql <<EOF
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+    mariadbd --user=mysql --bootstrap --console <<EOF
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 CREATE DATABASE ${WP_DB_NAME};
 CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';
@@ -8,3 +15,6 @@ FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
+else
+    mariadbd --user=mysql --console
+fi
--- srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
@@ -5,8 +5,8 @@ wp core download --locale=en_GB
 wp config create \
     --dbname=${WP_DB_NAME} \
     --dbuser=${WP_DB_USER} \
-    --prompt=${WP_DB_PASSWORD} \
-    --dbhost=${WP_DB_HOST} && echo "wp-config.php created"
+    --dbpass=${WP_DB_PASSWORD} \
+    --dbhost=mariadb && echo "wp-config.php created"
 wp db create && echo "Database created"
 wp core install \
     --url=${WP_URL} \
@@ -15,4 +15,4 @@ wp core install \
     --admin_password=${WP_ADMIN_PASSWORD} \
     --admin_email=${WP_ADMIN_EMAIL} && echo "Wordpress installed"
 
-tail -f /dev/null
+php-fpm${PHP_VERSION} -F
